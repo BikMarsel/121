62724c2758b1248523b90610e071382168b4073e
BikMarsel <mbikbaev3@gmail.com>
2024-08-07 23:21:41 +0500

    Add files via upload

---
+++ homework.py
@@ -0,0 +1,5 @@
+Kolichestvo_vipolnennih_DZ = 12
+Kolichestvo_zatrachennih_chasov = 1.5
+Nazvanie_kursa = 'Python'
+Vremia_na_odno_zadanie = Kolichestvo_zatrachennih_chasov/Kolichestvo_vipolnennih_DZ
+print("Курс:",Nazvanie_kursa,',', "всего задач:", Kolichestvo_vipolnennih_DZ,', '"затрачено часов:", Kolichestvo_zatrachennih_chasov,', ', "среднее время выполнения", Vremia_na_odno_zadanie,"часа.")
