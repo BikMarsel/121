064022daeeaedd4a3e0af27e101b6a6708a0700a
BikMarsel <mbikbaev3@gmail.com>
2024-08-08 23:04:51 +0500

    Add files via upload

---
+++ module_1_4.py
@@ -0,0 +1,9 @@
+my_string = input('Введите любой текст: ')
+print('Количество символов введённого текста,', len(my_string))
+
+print('my_string'.upper())
+print('my_string'.lower())
+print('my_string'.replace('_',''))
+name ='my_string'
+print(name [0])
+print(name [-1])
