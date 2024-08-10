0f9a17fc51334dc7afb022a47e408456ae3c1194
BikMarsel <mbikbaev3@gmail.com>
2024-08-11 00:44:30 +0500

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,13 @@
+# 2
+# immutable_var = (1, 2, 3, 'rock', True)
+# print(immutable_var)
+# 3
+# immutable_var [0] = 33
+# print(immutable_var) # Изменить значения элементов кортежа нельзя, потому что он относится к неизменяемым объектам.
+# 4
+mutable_list = [1, 2, 3, 'rock', True]
+print(mutable_list)
+mutable_list [0] = 33
+print(mutable_list)
+mutable_list [-1] ='Шиш'
+print(mutable_list)
