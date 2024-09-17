b8f53a50935fcf5717dc400b2824cda52026af52
BikMarsel <mbikbaev3@gmail.com>
2024-09-17 22:33:43 +0500

    Add files via upload

---
+++ fake_math.py
@@ -0,0 +1,10 @@
+def fake_divide(first, second):
+    if second == 0:
+        return "Ошибка"
+    return first/second
+
+
+result1 = fake_divide(69, 3)
+print(result1)
+result2 = fake_divide(3, 0)
+print(result2)
