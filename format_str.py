28ef550946ebe4b225a637ee890a447a79e0060c
BikMarsel <mbikbaev3@gmail.com>
2024-11-14 00:58:34 +0500

    Add files via upload

---
+++ format_str.py
@@ -0,0 +1,37 @@
+team1_num = 5
+team2_num = 6
+score_1 = 40
+score_2 = 42
+team1_time = 1552.512
+team2_time = 2153.31451
+tasks_total = 82
+time_avg = 45.2
+challenge_result = 'Победа команды Волшебники данных!'
+
+def challenge_result(team1_time=1552.512, team2_time=2153.31451, score_1=40, score_2=42):
+    if score_1 > score_2 or score_1 == score_2 and team1_time > team2_time:
+        result = 'Победа команды Мастера кода!'
+    elif score_1 < score_2 or score_1 == score_2 and team1_time < team2_time:
+        result = 'Победа команды Волшебники Данных!'
+    else:
+        result = 'Ничья!'
+    return result
+
+#def determinant_score():
+
+# Использование %:
+print('В команде Мастера кода участников: %s' % team1_num,'!', sep='')
+print('Итого сегодня в командах участников: %s' % team1_num,' и %s' % team2_num,'!', sep='')
+
+#Использование format():
+print('Команда Волшебники данных решила задач: {}!'.format(score_2))
+print('Волшебники данных решили задачи за {}'.format(team1_time),'с!')
+
+#Использование f-строк:
+print(f'Команды решили {score_1} и {score_2} задач.')
+print(f'Результат битвы: победа команды Мастера кода!')
+print(f'Сегодня было решено {score_1+score_2} задач, в среднем по {round((team1_time/score_1+team2_time/score_2)/2, 2)} секунды на задачу!')
+
+result = challenge_result()
+print(result)
+
