dffd4838efd24052b7be853b404357374c3a4cc6
BikMarsel <mbikbaev3@gmail.com>
2024-11-19 23:02:48 +0500

    Add files via upload

--- module_7.py
+++ module_7.py
@@ -3,9 +3,9 @@ import time
 
 for root, dirs, files in os.walk('test'):
     for file in files:
-        filepath = os.path.join('test')
+        filepath = os.path.join(root, file)
         filetime = os.path.getmtime('test')
         formatted_time = time.strftime("%d.%m.%Y %H:%M", time.localtime(filetime))
-        filesize = os.path.getsize(r'/Users/mbikb/OneDrive/Документы/GitHub/121/file_os/test/test.txt')
+        filesize = os.path.getsize(filepath)
         parent_dir = os.path.dirname(r'/Users/mbikb/OneDrive/Документы/GitHub/121/file_os/test')
     print(f'Обнаружен файл: {file}, Путь: {filepath}, Размер: {filesize} байт, Время изменения: {formatted_time},Родительская директория: {parent_dir}')
